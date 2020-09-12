e88701ab1837354859c66a17e79ea14b23dd96d5
Takahiro.Hori <brightholidoy610@gmail.com>
2020-09-12 07:15:59 +0000

    kakuninnsuru

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,21 @@
 class TasksController < ApplicationController
   before_action :set_task, only:[:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index, :show, :new, :create, :create, :edit, :update, :deatroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = current_user.task.all
+    @tasks = current_user.tasks.all
   end
 
   def show
   end
 
   def new
-    @task = cuurent.task.build 
+    @task = current_user.tasks.build 
   end
 
   def create
-    @task = current_user.task.build(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に登録されました'
@@ -51,10 +52,17 @@ class TasksController < ApplicationController
   # Strong Parameter
   
   def set_task
-    @task = current_user.task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+    def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  get 'signup', to: 'users#new'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] #これ全消しの可能性あり
 end
